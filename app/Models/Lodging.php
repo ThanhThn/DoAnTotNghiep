6ca40589d011bfa6d6de773005ee9031ce6be882
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-02-12 12:15:34 +0700

    api list lodging by user

--- app/Models/Lodging.php
+++ app/Models/Lodging.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lodging extends Model
 {
-    //
+    protected $table = 'lodgings';
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
 }
