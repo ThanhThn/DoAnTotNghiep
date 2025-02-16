91ae5b83824f1e13b3522f1325b56dca25b0a454
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-02-16 21:37:19 +0700

    api create lodging service

--- app/Http/Requests/LodgingService/CreateLodgingServiceRequest.php
+++ app/Http/Requests/LodgingService/CreateLodgingServiceRequest.php
@@ -9,8 +9,8 @@ class CreateLodgingServiceRequest extends BaseRequest
     public function rules(): array{
         return [
             'lodging_id' => 'required|uuid|exists:lodgings,id',
-            'service_name' => 'required_without:service_id|string',
-            'service_id' => 'required_without:service_name|integer|exists:services,id',
+            'service_name' => 'required_without:service_id|string|nullable',
+            'service_id' => 'required_without:service_name|integer|nullable|exists:services,id',
             'late_days' => 'nullable|integer|min:1',
             'payment_date' => 'nullable|integer|between:1,28',
             'unit_id' => 'required|integer|exists:units,id',
