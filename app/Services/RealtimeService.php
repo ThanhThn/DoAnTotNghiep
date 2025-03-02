5b77e9b8c2cdd61312df0e40de812191e1653e57
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-03-03 01:54:54 +0700

    fix

--- app/Services/RealtimeService.php
+++ app/Services/RealtimeService.php
@@ -3,10 +3,11 @@
 namespace App\Services;
 
 use Ably\AblyRest;
+use Illuminate\Support\Facades\Http;
 
 class RealtimeService
 {
-    function getToken($userId)
+    public function getToken($userId)
     {
         $ably = new AblyRest(env('ABLY_KEY'));
 
@@ -14,11 +15,21 @@ class RealtimeService
             "notification/user/$userId" => ["subscribe"]
         ];
 
+        $keyParts = explode(":", env('ABLY_KEY'));
+        $ablyKeyName = $keyParts[0];
+
         $tokenRequest = $ably->auth->createTokenRequest([
-            'capability' => json_encode($capability),
+            "capability" => json_encode($capability),
         ]);
 
-        return $tokenRequest;
+        $response = Http::post("https://rest.ably.io/keys/$ablyKeyName/requestToken", [
+            "keyName"    => $tokenRequest['keyName'],
+            "timestamp"  => $tokenRequest['timestamp'],
+            "nonce"      => $tokenRequest['nonce'],
+            "mac"        => $tokenRequest['mac'],
+            "capability" => $tokenRequest['capability'],
+        ]);
+        return $response->json();
     }
 
 }
