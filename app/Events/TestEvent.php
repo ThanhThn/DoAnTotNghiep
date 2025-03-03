066135a0a8d2a14e61fb9802880135f1709c5e26
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-03-03 16:19:42 +0700

    push  test

---
+++ app/Events/TestEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return ['test-channel'];
+    }
+
+    public function broadcastWith() : array
+    {
+        return [
+            "data" => $this->message
+        ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'TestEvent';
+    }
+}
+
