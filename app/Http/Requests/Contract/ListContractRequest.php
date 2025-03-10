a26b2eec129add0fa0ad66c3696bc657efb87ff7
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-03-10 13:25:51 +0700

    fix api list contract

--- app/Http/Requests/Contract/ListContractRequest.php
+++ app/Http/Requests/Contract/ListContractRequest.php
@@ -9,8 +9,8 @@ class ListContractRequest extends BaseRequest
 {
     public function rules(): array{
         return [
-            'lodging_id' => 'required|uuid|exists:lodging,id',
-            'room_id' => 'nullable|uuid|exists:room,id',
+            'lodging_id' => 'required|uuid|exists:lodgings,id',
+            'room_id' => 'nullable|uuid|exists:rooms,id',
             'status' => 'nullable|integer',
             'limit' => 'nullable|integer',
             'offset' => 'nullable|integer',
