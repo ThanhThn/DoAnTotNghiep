fe0a42ecb2823c69855c7fd85f4354d42c055699
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-03-18 15:17:02 +0700

    test with minetype

--- app/Http/Requests/Equipment/UpdateEquipmentRequest.php
+++ app/Http/Requests/Equipment/UpdateEquipmentRequest.php
@@ -6,16 +6,25 @@ use App\Http\Requests\BaseRequest;
 
 class UpdateEquipmentRequest extends BaseRequest
 {
-    public function rules(){
-        return [
+    public function rules()
+    {
+        $rules = [
             'id' => 'required|uuid|exists:equipments,id',
             'name' => 'required|string',
             'quantity' => 'required|integer',
             'type' => 'required|integer|in:1,2,3',
-            'thumbnail' => 'required|string|mimetypes:image/jpeg,image/png,image/webp',
             'lodging_id' => 'required|uuid|exists:lodgings,id',
             'room_ids' => 'required|array',
             'room_ids.*' => 'required|uuid|exists:rooms,id',
         ];
+
+        if ($this->hasFile('thumbnail')) {
+            $rules['thumbnail'] = 'file|mimes:jpeg,png,webp|max:5120';
+        } else {
+            $rules['thumbnail'] = 'string';
+        }
+
+        return $rules;
     }
+
 }
