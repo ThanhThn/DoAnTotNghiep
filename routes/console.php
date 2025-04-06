a15a2c71dd8551ef19111444bc797dca080cf0cd
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-04-06 22:45:10 +0700

    test run console

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Console\Scheduling\Schedule;
 
 
+Artisan::command('inspire', function () {
+   \Illuminate\Support\Facades\Log::info("RUN TEST: ", [\Carbon\Carbon::now()]);
+})->everyMinute();
+
 $schedule = app(Schedule::class);
 
 
 $schedule->command('rental:check-payments')->dailyAt('08:00');
-$schedule->command('service:check-payment')->dailyAt('08:00');
-$schedule->command('dashboard:change-total')->dailyAt('00:00');
+$schedule->command('service:check-payment')->dailyAt('08:05');
+$schedule->command('dashboard:change-total')->daily();
