5d2987030a0ea55f42d169f8ac01f07063f00d13
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-04-06 22:51:42 +0700

    test run console

--- routes/console.php
+++ routes/console.php
@@ -4,11 +4,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Console\Scheduling\Schedule;
 
-
-Artisan::command('inspire', function () {
-   \Illuminate\Support\Facades\Log::info("RUN TEST: ", [\Carbon\Carbon::now()]);
-})->everyMinute();
-
 $schedule = app(Schedule::class);
 
 
