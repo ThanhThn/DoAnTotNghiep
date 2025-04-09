6ddd6a3e295861622c1ff9569ff989d51ab2bda5
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2025-04-09 16:32:35 +0700

    test run console

--- app/Http/Requests/Contract/CreateFinalBillRequest.php
+++ app/Http/Requests/Contract/CreateFinalBillRequest.php
@@ -15,8 +15,8 @@ class CreateFinalBillRequest extends BaseRequest
             'end_date' => 'required|date',
             'is_monthly_billing' => 'nullable|boolean',
             'services' => 'nullable|array',
-            'services.id' => 'required_with:services|exists:lodging_services,id',
-            'services.value' => 'nullable|numeric|min:0',
+            'services.*.id' => 'required_with:services|exists:lodging_services,id',
+            'services.*.value' => 'nullable|numeric|min:0',
         ];
     }
 }
